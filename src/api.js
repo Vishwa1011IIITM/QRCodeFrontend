dadc1c171e47b6380078631d87f9ec5493c31d69
Vishwa1011 <vishupatel1011@gmail.com>
2024-12-06 17:32:36 +0530

    Deployment

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/',
+    baseURL: 'https://qrcodebackend-c4if.onrender.com',
     headers: { 'Content-Type': 'application/json' },
 });
 
